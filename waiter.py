3abed8f468d84236f3750bb0e25a9290a2363bce
guillermochussir <gchussir@gmail.com>
2018-07-14 19:31:36 -0300

    parametrizar Waiter

--- waiter.py
+++ waiter.py
@@ -1,15 +1,41 @@
 import json
+import sys
 from extractor import Extractor
 from pedido import Pedido
 
-def main(extractor,pedido):
+def LlevarPedido(parametro,extractor):
+    if parametro == "GetFundsProducts":
+        with open('urls.json') as f:
+            urls = json.load(f)
+        url = urls["GetFundsProducts"] + urls["GetFundsProductsParams"]
+    elif parametro == "GetPriceData":
+        with open('urls.json') as f:
+            urls = json.load(f)
+        url = urls["GetPriceData"]
+    elif parametro == "GetIndustryGroupPieData":
+        with open('urls.json') as f:
+            urls = json.load(f)
+        url = urls["GetIndustryGroupPieData"]
+    elif parametro == "GetIndustrySectorPieData":
+        with open('urls.json') as f:
+            urls = json.load(f)
+        url = urls["GetIndustrySectorPieData"]
+    elif parametro == "GetCompositionPieData":
+        with open('urls.json') as f:
+            urls = json.load(f)
+        url = urls["GetCompositionPieData"]
+    else:
+        print ("Perdón, lo que me pediste no está en el menu")
+        exit()
+    pedido = Pedido(url)
     extractor.extraer(pedido)
 
 
 if __name__ == '__main__':
-    _extractor = Extractor()
-    with open('urls.json') as f:
-        urls = json.load(f)
-    url = urls["GetFundsProducts"] + urls["GetFundsProductsParams"]
-    _pedido = Pedido(url)
-    main(_extractor,_pedido)
+    if len(sys.argv) == 1:
+        print ("Me podés pedir: GetFundsProducts, GetPriceData, GetIndustryGroupPieData, GetIndustrySectorPieData, GetCompositionPieData")
+    else:
+        _parametro = sys.argv[1] 
+        _extractor = Extractor()
+        print ("Sale {0} con fritas".format(_parametro))
+        LlevarPedido(_parametro,_extractor)
