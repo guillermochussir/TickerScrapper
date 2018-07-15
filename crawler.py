57550c0317b93ce4be39c822cfe55cb33a4f2cdf
fernando <fernando@spect.ro>
2018-07-15 13:02:14 -0300

    comments

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ import json
 class Crawler():
     #Extract market data from the API
     def __init__(self):
+        #API urls
         self.FundsProducts = "https://fondosonline.com/Operations/Funds/GetFundsProducts"
         self.FundsProductsParams = "?sortColumn=YearPercent&isAscending=false&PageSize=1000&searchFundName=&searchCurrency=-1&searchFocus=-1&searchStrategy=&searchHorizon=-1&searchProfile=-1&isActive=false&searchMinInvestment=&page=1"
         self.PriceData = "https://fondosonline.com/Information/FundData/GetPriceData?ticker={}"
@@ -13,6 +14,7 @@ class Crawler():
         self.CompositionPieData = "https://fondosonline.com/Information/FundData/GetCompositionPieData?ticker={}"
         
     def request(self, url):
+        #Get api response
         for tries in range(1, 4):
             try:
                 print (url)
@@ -28,10 +30,12 @@ class Crawler():
                     print ('tercer error.')
     
     def extract(self, url):
+        #Load api response as json
         records = json.loads(self.request(url))
         return records
                
     def getFunds(self):
+        #Get funds data as pandas df
         funds = self.extract(self.FundsProducts + self.FundsProductsParams)
         funds = funds["records"]
         df = pd.DataFrame.from_records(funds)
@@ -39,6 +43,7 @@ class Crawler():
         return df
     
     def getTicker(self,ticker):
+        #Get price data for the ticker. HEADS UP: no data a 0
         ticker = self.extract(self.PriceData.format(ticker))
         df = pd.DataFrame.from_records(ticker['fundValues'])
         if df is None:
@@ -53,6 +58,7 @@ class Crawler():
                 return 0
     
     def getPrices(self):
+        #Returns a df with funds data and a columns of dfs with prices for that fund
         funds = self.getFunds()
         funds['pricesDF'] = [self.getTicker(t) for t in funds['fundBloombergId']]
         return funds
